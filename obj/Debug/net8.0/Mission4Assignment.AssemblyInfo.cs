ce21ef884b6291dadd6934de60203a5f70a286f2
emmahelquist <154380901+emmahelquist@users.noreply.github.com>
2025-01-27 14:17:05 -0700

    Done!

--- obj/Debug/net8.0/Mission4Assignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mission4Assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission4Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f25ed8760c473e9f4163974ed3ec76384955143")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission4Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission4Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
